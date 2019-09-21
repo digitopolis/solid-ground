61ad07432c0b069483bf0fbd8d2ee784480f83fd
Matt Readout <matt.readout@gmail.com>
2019-09-21 17:05:02 -0400

    cycle through components on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 // import './App.css';
 import Breathe from './components/breathe'
+import Intro from './components/intro'
 
-function App() {
-  return (
-    <div className='main'>
-			<h1>Breathing exercise</h1>
-      <Breathe />
-    </div>
-  );
+class App extends React.Component {
+
+	state = {
+		number: 0
+	}
+
+	cycleContent = () => {
+		if (this.state.number < 4) {
+			const number = this.state.number + 1
+			this.setState({ number })
+		} else {
+			console.log('start over');
+		}
+	}
+
+	renderContent = () => {
+		switch (this.state.number) {
+			case 0:
+				return <Intro />
+			case 1:
+				return <Breathe />
+			default:
+				return <h1>the end</h1>
+		}
+	}
+
+	render() {
+		return (
+			<div className='main' onClick={this.cycleContent}>
+				{this.renderContent()}
+			</div>
+		);
+	}
+
 }
 
 export default App;
