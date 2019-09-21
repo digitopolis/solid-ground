f2fac207879a8f7dc4ec50e57f0a4d732256fb51
Matt Readout <matt.readout@gmail.com>
2019-09-21 17:04:00 -0400

    stub intro component

---
+++ src/components/intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainContainer from '../containers/mainContainer'
+
+const Intro = () => {
+	return (
+		<MainContainer>
+			<h1>This is the introduction</h1>
+		</MainContainer>
+	)
+}
+
+export default Intro
