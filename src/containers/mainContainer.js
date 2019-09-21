b0d5d4ff129adc69df0d1e3612208032e753f829
Matt Readout <matt.readout@gmail.com>
2019-09-21 16:51:11 -0400

    create container component

---
+++ src/containers/mainContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const mainContainer = (props) => {
+	return (
+		<Container>{props.children}</Container>
+	)
+}
+
+export default mainContainer
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	height: 450px;
+`
