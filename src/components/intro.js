5357bb41baacff44c6f9c7a240eb0c16dbb3c2d0
Matt Readout <matt.readout@gmail.com>
2019-09-22 17:18:01 -0400

    add intro text with animation

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import MainContainer from '../containers/mainContainer'
+import styled from 'styled-components'
 
 const Intro = () => {
 	return (
 		<MainContainer>
-			<h1>This is the introduction</h1>
+			<TextContainer>
+				<h1 className='content'>Anxiety is a common response our bodies have to stressful situations. </h1>
+				<h1 className='content'>You might experience rapid heart rate, sweating, and shortness of breath. </h1>
+				<h1 className='content'>This exercise can help you ground yourself in the present, and stop your mind from racing. </h1>
+				<h1 className='content'>Click anywhere to start, and first focus on taking some slow, deep breaths. </h1>
+			</TextContainer>
 		</MainContainer>
 	)
 }
 
 export default Intro
+
+const TextContainer = styled.div`
+	width: 60%;
+	text-align: center;
+	align-self: center;
+`
