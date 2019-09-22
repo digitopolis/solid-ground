980529921be3582b7483767e61c1984df4842d8c
Matt Readout <matt.readout@gmail.com>
2019-09-22 17:51:23 -0400

    make input styling global

--- src/components/see.js
+++ src/components/see.js
@@ -1,29 +1,19 @@
 import React from 'react'
 import MainContainer from '../containers/mainContainer'
-import styled from 'styled-components'
 
 const See = () => {
 	return (
 		<MainContainer>
 			<h1>Identify FIVE things you can see around you.</h1>
 			<form>
-				<Input /><br/>
-				<Input /><br/>
-				<Input /><br/>
-				<Input /><br/>
-				<Input /><br/>
+				<input /><br/>
+				<input /><br/>
+				<input /><br/>
+				<input /><br/>
+				<input /><br/>
 			</form>
 		</MainContainer>
 	)
 }
 
 export default See
-
-const Input = styled.input`
-	font-size: 1em;
-	border: 2px solid black;
-	border-radius: 3px;
-	width: 300px;
-	padding: .5em;
-	margin: 1em;
-`
