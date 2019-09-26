ae4ea492c0b5ce79fe47a7d6e7c6b3dcc2155960
Matt Readout <matt.readout@gmail.com>
2019-09-26 17:08:25 -0400

    build data object with headlines

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const DATA = {
+	headlines: {
+		see: 'Identify FIVE things you can see around you.',
+		touch: 'Identify FOUR things you can touch around you.',
+		hear: 'Identify THREE things you can hear around you.',
+		smell: 'Identify TWO things you can smell around you.',
+		taste: 'Identify ONE thing you can taste.'
+	}
+}
