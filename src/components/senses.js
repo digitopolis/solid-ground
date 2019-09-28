bffaabaa31fb38664f4db06db183529daaf93a8e
Matt Readout <matt.readout@gmail.com>
2019-09-28 16:30:16 -0400

    add continue button to senses

--- src/components/senses.js
+++ src/components/senses.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import MainContainer from '../containers/mainContainer'
+import styled from 'styled-components'
 
 const Senses = (props) => {
 	const inputArray = []
 	for (var i = 0; i < props.number; i++) {
-		inputArray.push(<><input /><br/></>)
+		inputArray.push(<><input key={i}/><br/></>)
 	}
 	return (
 		<MainContainer>
@@ -13,9 +14,20 @@ const Senses = (props) => {
 				{inputArray.map(input => {
 					return input
 				})}
+				<Button onClick={props.advance}>Continue</Button>
 			</form>
 		</MainContainer>
 	)
 }
 
 export default Senses
+
+const Button = styled.button`
+	background: black;
+	color: white;
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid black;
+	border-radius: 3px;
+`
